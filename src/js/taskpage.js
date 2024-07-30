40b53ca737120e4e203b62b6c10a847b0fcaf50e
Emel Mayorga <emayorga@fwdcostarica.com>
2024-07-30 15:54:51 -0600

    tercerAvance

--- src/js/taskpage.js
+++ src/js/taskpage.js
@@ -6,37 +6,32 @@ const eventList =document.getElementById("eventList");
 const selector = document.getElementById("selector");
 const taskPriority = document.getElementById("taskPriority");
 
-let textListTask = JSON.parse(localStorage.getItem("taskText"))||[];
-let textListEvent = JSON.parse(localStorage.getItem("eventText"))||[];
-
-function renderList() {
-    textListEvent.forEach((taskItem, index) => renderItem (taskItem, index, "Task"));
-    textListEvent.array.forEach((eventItem, index) => renderItem(eventItem, index, "Event"));
-}
-
+let textListTask =  [];
+let textListEvent = [];
 
 btnS.addEventListener("click", function () {
     const selection = selector.value
     localStorage.clear();
 
     if (selection === 'Task') {
-        const containerN =document.createElement("div")
-        const pTag = document.createElement("p");
-        pTag.id ="TextoP"
-        const btnD = document.createElement("button");
-        const btnE = document.createElement("button");
-        containerN.appendChild(pTag);
-        containerN.appendChild(btnD);
-        containerN.appendChild(btnE);
-        let taskConca = textM.value +" "+ taskPriority.value;
-        pTag.innerHTML= taskConca
-        btnD.innerHTML = "Delete Tasks"; 
-        btnE.innerHTML = "Edit";
-        taskList.appendChild(containerN);
-        textListTask.push(taskConca);
-        localStorage.setItem("taskText", JSON.stringify(textListTask));
+            const containerN =document.createElement("div")
+            const pTag = document.createElement("p");
+            pTag.id ="TextoP"
+            const btnD = document.createElement("button");
+            const btnE = document.createElement("button");
+            containerN.appendChild(pTag);
+            containerN.appendChild(btnD);
+            containerN.appendChild(btnE);
+            let taskConca = textM.value +" "+ taskPriority.value;
+            pTag.innerHTML= taskConca
+            btnD.innerHTML = "Delete Tasks"; 
+            btnE.innerHTML = "Edit";
+            taskList.appendChild(containerN);
+            textListTask.push(taskConca);
+            localStorage.setItem("taskText", JSON.stringify(textListTask));
 
         if (btnD.addEventListener("click", function () {
+            textListTask.filter(textListTask);
             taskList.removeChild(containerN);
             localStorage.setItem("taskText", JSON.stringify(textListTask));
             alert("Deleted Succesfully");
@@ -44,7 +39,7 @@ btnS.addEventListener("click", function () {
         
         if (btnE.addEventListener("click", function () {
             const btnA = document.createElement("button");
-            containerN.appendChild(btnA);
+            containerN.appendChild(btnA); 
             btnA.innerHTML = "Apply";
             pTag.contentEditable = true;
             pTag.style.backgroundColor = "#666";
@@ -54,9 +49,10 @@ btnS.addEventListener("click", function () {
             pTag.contentEditable = false;
             pTag.style.backgroundColor = "White";
             pTag.style.color = "Black";
+            this.after(btnA.remove())
         })){}
         })){}
-        
+         
     } else { if (selection === 'Event') {
         const containerN =document.createElement("div")
         const pTag = document.createElement("p");
@@ -76,6 +72,7 @@ btnS.addEventListener("click", function () {
         
 
         if (btnD.addEventListener("click", function () {
+            let evenListDeleted = textListEvent.JSON.parse(getItem)
             eventList.removeChild(containerN);
             alert("Deleted Succesfully");
         })) {}
@@ -88,18 +85,15 @@ btnS.addEventListener("click", function () {
             pTag.style.backgroundColor = "#666";
             pTag.style.color = "white";
 
-            if (div ) {
+
                 if (btnA.addEventListener("click", function () {
                     pTag.contentEditable = false;
                     pTag.style.backgroundColor = "White";
                     pTag.style.color = "Black";
-        
+                    this.after(btnA.remove());
                 })){}
-            } else {
-                
-            }
+             
 
 
         })){}
     } }})
-renderList();
